2821a58424f1275534204da8d9aef98244c4ad40
SaidazizxonFlutterDev <smister702@gmail.com>
2022-03-30 10:25:33 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:halol_farm/route/route_generator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: (s) => RouteGenerator.generateRoute(s),
+    );
+  }
+}
+
---
+++ lib/route/route_generator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:halol_farm/screens/my_home_page.dart';
+
+class RouteGenerator {
+  static generateRoute (RouteSettings s){
+    var args = s.arguments;
+    switch (s.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) =>const MyHomePage(),
+        );
+        
+    }
+  }
+}
